d55f5464af7ea453609fa9a6cb236c4d5e1897ee
Tunjay Akbarli <128295620+Tuncayscript@users.noreply.github.com>
2024-07-14 15:35:01 +0400

    Delete cmake directory

--- cmake/NeXTCodeVersion.cmake
+++
@@ -1,7 +0,0 @@
-# NEXTCODE_VERSION is deliberately /not/ cached so that an existing build directory
-# can be reused when a new version of NeXTCode comes out (assuming the user hasn't
-# manually set it as part of their own CMake configuration).
-set(NEXTCODE_VERSION_MAJOR 6)
-set(NEXTCODE_VERSION_MINOR 0)
-set(NEXTCODE_VERSION "${NEXTCODE_VERSION_MAJOR}.${NEXTCODE_VERSION_MINOR}")
-
--- cmake/modules/NeXTCodeCXXUtils.cmake
+++
@@ -1,6 +0,0 @@
-# Platforms that use libstdc++ as the system-wide default C++ standard library.
-set(NEXTCODE_LIBSTDCXX_PLATFORMS
-    "LINUX"
-    "FREEBSD"
-    "CYGWIN"
-    "HAIKU")
